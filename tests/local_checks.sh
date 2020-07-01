06a9a7f9ec75181d38b5ad2ee4ed10f145682d80
Alexey <alexeybe@checkpoint.com>
2020-07-01 14:48:30 +0300

    version + commands

--- tests/local_checks.sh
+++ tests/local_checks.sh
@@ -2,10 +2,25 @@
 
 
 
-sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket test-control-tower-integration --profile control-tower-integration-alexey
 
-#build local
-sam build
+sam build --profile control-tower-integration-alexey
 
 sam local invoke Dome9AutomationLambda --event ./event_account_creation.json
+
+
+sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket test-control-tower-integration --profile control-tower-integration-alexey
+
+sam publish     --template packaged.yaml     --region us-east-1 --profile control-tower-integration-alexey
+
+
+
+
+
+
+
+
+
+
+
+
 
